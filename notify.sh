0531d9498b38fa53662eed08c60c6459bbb3ac82
GaryHjy <782316959@qq.com>
2019-09-11 16:47:17 +0800

    ci: 修改notify.sh命令
    修改notify.sh命令

--- notify.sh
+++ notify.sh
@@ -12,8 +12,9 @@ curl -H "Authorization: token $GITHUB_TOKEN" $url > $resp_tmp_file
 
 html_url=$(sed -n 5p $resp_tmp_file | sed 's/\"html_url\"://g' | awk -F '"' '{print $2}')
 body=$(grep body < $resp_tmp_file | sed 's/\"body\"://g;s/\"//g')
+tag=$(grep body < $resp_tmp_file | sed 's/\"tag_name\"://g;s/\"//g')
 
-msg='{"msgtype": "markdown", "markdown": {"title": "admin-template-test更新", "text": "@所有人\n# [admin-template-test]('$html_url')\n'$body'"}}'
+msg='{"msgtype": "markdown", "markdown": {"title": "admin-template-test更新", "text": "@所有人\n# [admin-template-test]('$html_url')\n发布'$tag'版本"}}'
 
 curl -X POST https://oapi.dingtalk.com/robot/send\?access_token\=$DINGTALK_ROBOT_TOKEN -H 'Content-Type: application/json' -d "$msg"
 
